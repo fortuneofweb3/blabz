9c262199d554657e5b1aa7ffe9c9444a4c20a708
fortuneofweb3 <138443589+fortuneofweb3@users.noreply.github.com>
2025-06-13 01:48:41 +0100

    Update ProcessedPost.js

--- models/ProcessedPost.js
+++ models/ProcessedPost.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
-
 const processedPostSchema = new mongoose.Schema({
   postId: { type: String, required: true, unique: true },
   processedAt: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('ProcessedPost', processedPostSchema);
