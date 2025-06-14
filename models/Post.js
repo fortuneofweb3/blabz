2d4bb7ad0eec8086443ca1f740c07110770139ba
fortuneofweb3 <138443589+fortuneofweb3@users.noreply.github.com>
2025-06-14 04:05:00 +0100

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // Links to user
-  username: { type: String, required: true }, // Stores username for display
+  userId: { type: String, required: true },
+  username: { type: String, required: true },
   postId: { type: String, required: true, unique: true },
   content: String,
-  project: String,
+  project: [String],
   score: Number,
   likes: Number,
   retweets: Number,
-  replies: Number, // Added for comments
+  replies: Number,
   hashtags: [String],
   createdAt: Date,
   additionalFields: Object
