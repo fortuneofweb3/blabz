d2593e8c6f81dd81d2ca5c931ca52e907d258cb5
fortuneofweb3 <138443589+fortuneofweb3@users.noreply.github.com>
2025-06-15 03:43:09 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,17 +1,53 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // Twitter ID
-  USER_ID: { type: String, required: true, unique: true }, // Custom unique user ID
-  username: { type: String, required: true, unique: true },
-  name: { type: String, default: '' },
-  profile_image_url: { type: String, default: '' },
-  followers_count: { type: Number, default: 0 },
-  following_count: { type: Number, default: 0 },
-  bio: { type: String, default: '' },
-  location: { type: String, default: '' },
-  created_at: { type: Date },
-  additionalFields: { type: Object, default: {} }
-}, { timestamps: true });
+  SOL_ID: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    default: ''
+  },
+  profile_image_url: {
+    type: String,
+    default: ''
+  },
+  followers_count: {
+    type: Number,
+    default: 0
+  },
+  following_count: {
+    type: Number,
+    default: 0
+  },
+  bio: {
+    type: String,
+    default: ''
+  },
+  location: {
+    type: String,
+    default: ''
+  },
+  created_at: {
+    type: Date
+  },
+  additionalFields: {
+    type: Object,
+    default: {}
+  }
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model('User', userSchema);
