2c434bbc753cc2dd30ad9f04fcd89aa284037ab5
fortuneofweb3 <138443589+fortuneofweb3@users.noreply.github.com>
2025-06-27 09:58:29 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   SOL_ID: { type: String, required: true, unique: true },
   DEV_ID: { type: String, required: true, unique: true },
   userId: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, index: true },
   name: { type: String, default: '' },
   profile_image_url: { type: String, default: '' },
   followers_count: { type: Number, default: 0 },
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema({
   bio: { type: String, default: '' },
   location: { type: String, default: '' },
   created_at: { type: Date },
-  additionalFields: { type: mongoose.Schema.Types.Mixed, default: {} }
+  updated_at: { type: Date, default: Date.now }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
