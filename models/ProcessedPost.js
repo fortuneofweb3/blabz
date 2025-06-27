4db8a5de14a78b3746de601c7143533b02c83dd4
fortuneofweb3 <138443589+fortuneofweb3@users.noreply.github.com>
2025-06-27 17:18:37 +0100

    Delete models directory

--- models/ProcessedPost.js
+++
@@ -1,8 +0,0 @@
-// models/ProcessedPost.js
-const mongoose = require('mongoose');
-
-const ProcessedPostSchema = new mongoose.Schema({
-  postId: { type: String, required: true, unique: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('ProcessedPost', ProcessedPostSchema);
